3cb508926b858e80cc0d53cb62bbb506e394a972
missus <fkarolin@gmail.com>
2017-09-06 14:47:38 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Tuning-Bar" 
+"A simple single screen app about my favorite burger bar." 
---
+++ app/src/main/java/com/example/android/tuningbar/MainActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Created by Karolin Fornet.
+ * Copyright (c) 2017.  All rights reserved.
+ */
+
+package com.example.android.tuningbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
